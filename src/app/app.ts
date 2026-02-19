12ff55b49f9af5429383f62e669f0d584b4d59ff
Azurly <azerly.hn@gmail.com>
2026-02-19 11:40:56 +0100

    setting the first page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Sidebar } from "./components/sidebar/sidebar";
+import { Searchbar } from "./components/searchbar/searchbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Sidebar, Searchbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
